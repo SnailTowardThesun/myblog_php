287660b794c6aada3ae284558117193cb1968991
hankun <hanvskun@hotmail.com>
2017-01-13 15:24:56 +0800

    add favicon and footer

---
+++ trunk/template/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" align="center">
+                <p>Copyright &copy; 2016-2017 hanvskun@hotamil.com All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
