c134bfb0ac7b74a9d8f256f68b15ba625159dc32
hankun <hanvskun@hotmail.com>
2017-02-07 10:59:20 +0800

    store the post payload into file

--- trunk/github_hook.php
+++ trunk/github_hook.php
@@ -1,2 +1,19 @@
 <?php
-echo "github hook";
+
+$raw_data = file_get_contents('php://input');
+if (!empty($raw_data))
+{
+    echo "ready to write";
+    $file_path = dirname(__FILE__)."/github.log";
+
+    $file = fopen($file_path, "a");
+
+    fwrite($file, $raw_data);
+
+    fclose($file);
+}
+else
+{
+    echo "write nothing";
+}
+
